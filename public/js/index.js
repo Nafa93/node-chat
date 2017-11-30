e909f56be7f6fc4eb985070d5ad7aa1966f5c88c
Nicolas Fernandez Amorosino <>
2017-11-30 11:34:23 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to the server');
 
-    socket.emit('createMessage', {
-        to: 'jen@example.com',
-        text: 'Hey. This is Andrew'
-    });
+    // socket.emit('createMessage', {
+    //     to: 'jen@example.com',
+    //     text: 'Hey. This is Andrew'
+    // });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'mike@example.com',
-        text: 'Hey, what is going on?',
-        createAt: 123
-    });
+    // socket.emit('newMessage', {
+    //     from: 'mike@example.com',
+    //     text: 'Hey, what is going on?',
+    //     createAt: 123
+    // });
 
     socket.on('createMessage', (message) => {
-        console.log('createMessage', message)
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.to,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     socket.on('disconnect', () => {
